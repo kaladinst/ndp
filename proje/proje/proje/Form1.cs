6c652db116e74cf410264e1a3fc39581721ff8df
emir <145506836+kaladinst@users.noreply.github.com>
2024-05-20 19:13:50 +0300

    Add files via upload

---
+++ proje/proje/proje/Form1.cs
@@ -0,0 +1,10 @@
+namespace proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ proje/proje/proje/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ContactInfo { get; set; }
+    }
+    public interface IAppointment
+    {
+        void Schedule();
+        void Cancel();
+    }
+    public class Customer : Person
+    {
+
+    }
+    // Define the Employee class
+    public class Employee : Person
+    {
+
+    }
+
+// Define the Service class
+    public class Service
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+
+
+// Define the Appointment class
+    public class Appointment : IAppointment
+    {
+        public Customer Customer { get; set; }
+        public Service Service { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime Date { get; set; }
+
+        public void Schedule()
+        {
+        }
+
+        public void Cancel()
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
